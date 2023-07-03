34ee840b9ef53490cf56a78a1d2fc7c1fa1c383c
JustATangMan <jasontang127@gmail.com>
2023-07-03 15:59:11 -0400

    first commit

---
+++ src/test/java/com/jtang/springboot/biz/BizExpenseReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtang.springboot.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizExpenseReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
